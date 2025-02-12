5718d2371f883935e964d79c6df183b31990c93e
pnumin <pnumin@pusan.ac.kr>
2025-02-12 12:28:03 +0900

    자바스크립트 배열열

--- 05/05.js
+++ 05/05.js
@@ -2,20 +2,24 @@ function showMsg(m) {
   document.getElementById('txt2').value = m;
 }
 
-
 function check1() { 
   //1. 입력문자열 가져오기
   let s = document.getElementById('txt1').value ;
+  console.log(s) ;
 
   //2. 문자열 뒤집어진 문자열 생성 
   let sr = '' ;
-  for(let i = s.length - 1 ; i >= 0; i--) {
-    sr = sr + s[i] ;
-  }
-  console.log(s, sr);
+  // for(let i = s.length - 1 ; i >= 0; i--) {
+  //   sr = sr + s[i] ;
+  // }
+  // console.log(s, sr);
+  //2-1. 배열을 이용하여 문자열 뒤집기
+  sr = s.split('').reverse().join('');
+  console.log(sr);
+  console.log(`sr = ${sr}`)
 
   //3. 입력문자열 뒤집어진 문자열을 비교
-  if (s === sr) {
+  if (s.replaceAll(' ', '') === sr.replaceAll(' ','')) {
     showMsg('회문입니다.');
   }
   else {
