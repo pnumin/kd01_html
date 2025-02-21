a56136e45d9c41b2848aaa4af7c106bca9db28b8
pnumin <pnumin@pusan.ac.kr>
2025-02-21 10:44:07 +0900

    일일박스오피스스

--- 13/13.js
+++ 13/13.js
@@ -1,40 +1,27 @@
-const getFetch = async(dt, ul) => {
-  let apiKey = '' ;
-  let baseUrl = 'http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?' ;
-  let url = `${baseUrl}key=${apiKey}&targetDt=${dt}` ;
-  console.log('getFetch',url);
-  // fetch API 
-  try{
-    const resp = await fetch(url) ; 
-    const data = await resp.json() ;
-    console.log(data.boxOfficeResult.dailyBoxOfficeList)
 
-    let tags = '' ;
-    for(let item of data.boxOfficeResult.dailyBoxOfficeList) {
-      tags = tags + 
-          `<li>
-            <span class="sp">${item.rank}</span>
-            ${item.movieNm}
-          </li>` ;
-    }
-    ul.innerHTML = tags ;
+const getYesterday = () => {
+  let dt = new Date() ;
+  dt.setDate(dt.getDate() - 1) ;
 
-  } catch(err) {
-    console.log(err)
-  }
-  
+  //년도
+  let year = dt.getFullYear() ;
+
+  //월
+  let month = String(dt.getMonth() + 1).padStart(2, '0') ;
+  // month = month < 10 ? '0' + month : month ;
+
+  //일 
+  let day = String(dt.getDate()).padStart(2,'0') ;
+
+  return (year + '-' + month + '-' + day);
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{
-  //필요한 요소 가지고 오기
-  const dt = document.querySelector('#dt') ;
-  const ul = document.querySelector('ul') ;
+  const inputDate = document.querySelector('[type=date]') ;
+  const radios = document.querySelectorAll('[type=radio]') ;
 
-  dt.addEventListener('change', (e)=>{
-    e.preventDefault();
-
-    console.log(dt.value);
-    getFetch(dt.value.replaceAll('-', ''), ul) ;
-  });
-
+  //어제 날짜 
+  const yesterday = getYesterday() ;
+  inputDate.max = yesterday ;
+  console.log(yesterday);
 });
