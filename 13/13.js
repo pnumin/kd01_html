0755e504a584c30292848ba8bb9f3ce61ffc0b7c
pnumin <pnumin@pusan.ac.kr>
2025-02-21 12:38:56 +0900

    일일박스오피스스

--- 13/13.js
+++ 13/13.js
@@ -25,6 +25,9 @@ const getPoster = async(movieNm) => {
 
 //박스오피스 가져오기
 const getBoxOfficeList = async() => {
+  const ul = document.querySelector('ul') ;
+  const mvPoster = document.querySelector('#mvPoster') ;
+
   const mvType = document.querySelector('[type=radio]:checked').value ;
   const dt = document.querySelector('[type=date]').value.replaceAll('-','') ;
   console.log("mvType", mvType, dt)
@@ -39,14 +42,30 @@ const getBoxOfficeList = async() => {
   //fetch
   const resp = await fetch(url);
   const data = await resp.json() ;
-  const dailyBoxOfficeList = data.boxOfficeResult.dailyBoxOfficeList ;
+
+  // console.log(data)
+  let dailyBoxOfficeList = data.boxOfficeResult.dailyBoxOfficeList ;
 
   let tags = '' ;
-  for (let boxoffice of dailyBoxOfficeList) {
-
-    await getPoster(boxoffice.movieNm) ;
+  for (let item of dailyBoxOfficeList) {
+    tags = tags + 
+          `<li>
+            <span class="sp">${item.rank}</span>
+            ${item.movieNm}
+          </li>` ;
+    await getPoster(item.movieNm) ;
   }
+  ul.innerHTML = tags ;
 
+  const lis = document.querySelectorAll('li') ;
+  for(let [i, li] of lis.entries()) {
+    li.addEventListener('mouseenter', () => {
+      posterArr[i] == '' 
+      ? mvPoster.innerHTML = "자료없음"
+      : mvPoster.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${posterArr[i]}">`;
+    })
+  }
+  console.log(lis) 
 
   console.log("posterArr",posterArr);
 }
@@ -74,6 +93,7 @@ const getYesterday = () => {
 document.addEventListener('DOMContentLoaded', ()=>{
   const inputDate = document.querySelector('[type=date]') ;
   const radios = document.querySelectorAll('[type=radio]') ;
+
   const bt = document.querySelector('button') ;
   
   //어제 날짜 
